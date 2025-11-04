f35613f819a5442a2741c3bdfb86e4da64c107b3
Basir Naji <basirnaji007@gmail.com>
2025-11-04 15:00:51 +0430

    post and get

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello from the server side...');
+  // res.status(200).send('Hello from the server side!');
+  res.status(200).json({ message: 'Hello from server side!', methode: 'get' });
+});
+app.post('/', (req, res) => {
+  // res.send('Send');
+
+  // res.status(404).json({ message: '404 from server', methode: 'post' });
+  res.status(200).json({ message: 'Hello world from server', methode: 'post' });
 });
 
 const port = 3000;
