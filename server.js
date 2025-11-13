412fb8f6aca7a78f466ea3dbefd19f6e0835dae4
Basir Naji <basirnaji007@gmail.com>
2025-11-13 21:35:32 +0430

    config.env created

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+const dotenv = require('dotenv');
 const app = require('./app');
-const port = 3000;
+
+dotenv.config({ path: './config.env' });
+const port = process.env.PORT || 3000;
+
+console.log(process.env);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
