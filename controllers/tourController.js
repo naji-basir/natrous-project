a8661de2025e61073a7e45c3609def175899af67
Basir Naji <basirnaji007@gmail.com>
2025-11-16 00:01:44 +0430

    Modeling the tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -46,7 +46,7 @@ exports.createTour = async (req, res) => {
       data: { tour: newTour },
     });
   } catch (error) {
-    res.status(400).json({ status: 'fail', message: 'Invalid data sent!' });
+    res.status(400).json({ status: 'fail', message: error });
   }
 };
 
