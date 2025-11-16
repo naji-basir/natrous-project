98783624c5c7b54991c615d94f7753fed6a61bdb
Basir Naji <basirnaji007@gmail.com>
2025-11-16 21:17:01 +0430

    filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -12,15 +12,41 @@ exports.checkBody = (req, res, next) => {
 //♾️
 exports.getallTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // 1) Filtering
+    const queryObj = { ...req.query };
+    const excludeFields = ['page', 'sort', 'limit', 'fields'];
+    excludeFields.forEach((el) => delete queryObj[el]);
+
+    // 2) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let finalQueryObj = JSON.parse(queryStr);
+
+    // Convert numeric strings to numbers
+    Object.keys(finalQueryObj).forEach((key) => {
+      if (
+        typeof finalQueryObj[key] === 'string' &&
+        !isNaN(finalQueryObj[key])
+      ) {
+        finalQueryObj[key] = Number(finalQueryObj[key]);
+      }
+    });
+
+    // 3) EXECUTE QUERY with the transformed object
+    const tours = await Tour.find(finalQueryObj);
+
+    // 4) SEND RESPONSE
     res.status(200).json({
       status: 'success',
-      result: tours.length,
-      requestedAt: req.requestTime,
+      results: tours.length,
       data: { tours },
     });
   } catch (error) {
-    res.status(400).json({ status: 'fail', message: error });
+    res.status(400).json({
+      status: 'fail',
+      message: error.message,
+    });
   }
 };
 
